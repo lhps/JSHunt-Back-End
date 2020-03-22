7e84bc76ff6d6ffc6f2779a35f517f881ef13ea8
Lucas Pinho <pinho@MacBook-Pro-de-Lucas.local>
2020-03-22 19:46:27 -0300

    Ajustes .env

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cors = require('cors')
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const dotenv = require('dotenv');
 
+dotenv.config();
 
 // Iniciando o App
 const app = express();
@@ -11,7 +13,7 @@ app.use(cors());
 
 // Iniciando o DB
 mongoose.connect(
-  'mongodb+srv://admin:admin@omnistack-b89wo.mongodb.net/hunt-api?retryWrites=true&w=majority',
+  `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PW}@omnistack-b89wo.mongodb.net/hunt-api?retryWrites=true&w=majority`,
    { useNewUrlParser: true,
       useUnifiedTopology: true },
    
